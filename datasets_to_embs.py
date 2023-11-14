30939843743ea27524bc2b5cd53393378515d8dd
Yuichi Tateno <hotchpotch@gmail.com>
2023-11-15 08:37:57 +0900

    Add formatter class to argparse and update load_dataset call

--- datasets_to_embs.py
+++ datasets_to_embs.py
@@ -11,7 +11,7 @@ from datasets import load_dataset
 
 parser = argparse.ArgumentParser(
     description="Convert datasets to embeddings",
-formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 
 parser.add_argument(
@@ -98,20 +98,24 @@ if "-e5-" in args.model_name:
     if args.passage_prefix not in passage_prefixes_e5:
         raise ValueError("passage_prefix should be one of", passage_prefixes_e5)
 
-target_ds = load_dataset(args.target_dataset, 'passages-c400-jawiki-20230403', split='train')
+target_ds = load_dataset(
+    args.target_dataset, "passages-c400-jawiki-20230403", split="train"
+)
+
 
 def data_to_passage(data, template=TEMPLATE, prefix=PASSAGE_PREFIX):
-    title = data['title']
-    section = data['section'] 
+    title = data["title"]
+    section = data["section"]
     if section == "__LEAD__":
         section = "概要"
-    text = data['text']
+    text = data["text"]
     formatted = template.format(title=title, section=section, text=text)
     return prefix + formatted
 
+
 print("---- example formatted passages ----")
 for i in range(3):
-    print(data_to_passage(target_ds[i])) # type: ignore
+    print(data_to_passage(target_ds[i]))  # type: ignore
     print("-" * 20)
 
 emb_config = EmbConfig(
@@ -132,7 +136,9 @@ if "-e5-" in model_name_for_embs_dir:
         raise ValueError("passage_prefix should be one of", passage_prefixes_e5)
 
 
-working_dir_embs_path = Path("/".join([args.working_dir, embs_dir, args.name, model_name_for_embs_dir]))
+working_dir_embs_path = Path(
+    "/".join([args.working_dir, embs_dir, args.name, model_name_for_embs_dir])
+)
 working_dir_embs_path.mkdir(parents=True, exist_ok=True)
 
 print("output embs path:", working_dir_embs_path)
@@ -141,7 +147,9 @@ MODEL = SentenceTransformer(emb_config.model_name)
 MODEL.max_seq_length = emb_config.max_seq_length
 
 
-def to_embs(texts: list[str], group_size=1024, model=MODEL) -> Generator[np.ndarray, None, None]:
+def to_embs(
+    texts: list[str], group_size=1024, model=MODEL
+) -> Generator[np.ndarray, None, None]:
     group = []
     for text in texts:
         group.append(text)
@@ -158,6 +166,7 @@ def to_embs(texts: list[str], group_size=1024, model=MODEL) -> Generator[np.ndar
             group, normalize_embeddings=True, show_progress_bar=False
         )
         yield embeddings  # type: ignore
+
 
 def ds_to_embs(
     ds,
@@ -185,13 +194,13 @@ def ds_to_embs(
 if torch.cuda.is_available():
     print("use cuda")
     MODEL.to("cuda")
-elif torch.backends.mps.is_available(): # type: ignore
+elif torch.backends.mps.is_available():  # type: ignore
     print("use mps (apple selicon)")
     MODEL.to("mps")
 else:
     print("!! Warning: use cpu")
 
-ds = target_ds # type: ignore
+ds = target_ds  # type: ignore
 
 if args.debug:
     print("debug mode")
@@ -199,7 +208,7 @@ if args.debug:
     print("small dataset len:", len(ds))
     group_size = 10_000
 else:
-    print("dataset len:", len(ds)) # type: ignore
+    print("dataset len:", len(ds))  # type: ignore
     group_size = 100_000
 
 for embs, idx, pbar in ds_to_embs(ds, data_to_passage, group_size=group_size):
