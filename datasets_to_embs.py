0e1200dc91266b1d552d2bcd56def873d1f2d448
Yuichi Tateno <hotchpotch@gmail.com>
2023-11-14 13:28:17 +0900

    Update argument names and output directory

--- datasets_to_embs.py
+++ datasets_to_embs.py
@@ -66,11 +66,11 @@ parser.add_argument(
 )
 # output_name
 parser.add_argument(
-    "-o",
-    "--output_dir",
+    "-w",
+    "--working_dir",
     type=str,
     default="outputs",
-    help="output dir name",
+    help="working_dir dir",
 )
 args = parser.parse_args()
 
@@ -88,14 +88,15 @@ args = parser.parse_args()
 TEMPLATE = args.passage_template
 PASSAGE_PREFIX = args.passage_prefix
 
+passage_prefixes_e5 = ("query: ", "passage: ")
+
 if "-e5-" in args.model_name:
-    passage_prefixes_e5 = ("query: ", "passage: ")
     if args.passage_prefix not in passage_prefixes_e5:
         raise ValueError("passage_prefix should be one of", passage_prefixes_e5)
 
 target_ds = load_dataset(args.target_dataset, 'passages-c400-jawiki-20230403', split='train')
 
-def passage_text(data, template=TEMPLATE, prefix=PASSAGE_PREFIX):
+def data_to_passage(data, template=TEMPLATE, prefix=PASSAGE_PREFIX):
     title = data['title']
     section = data['section'] 
     if section == "__LEAD__":
@@ -106,10 +107,8 @@ def passage_text(data, template=TEMPLATE, prefix=PASSAGE_PREFIX):
 
 print("---- example formatted passages ----")
 for i in range(3):
-    print(passage_text(target_ds[i])) # type: ignore
+    print(data_to_passage(target_ds[i])) # type: ignore
     print("-" * 20)
-
-exit()
 
 emb_config = EmbConfig(
     model_name=args.model_name,
@@ -118,21 +117,32 @@ emb_config = EmbConfig(
 )
 embs_dir = f"embs{'_debug' if args.debug else ''}"
 
-output_embs_path = Path("/".join([args.output_name, embs_dir, args.name]))
-output_embs_path.mkdir(parents=True, exist_ok=True)
+model_name_for_embs_dir = emb_config.model_name.split("/")[-1]
 
-print("output embs path:", output_embs_path)
+if "-e5-" in model_name_for_embs_dir:
+    if emb_config.passage_prefix == "query: ":
+        model_name_for_embs_dir += "-query"
+    elif emb_config.passage_prefix == "passage: ":
+        model_name_for_embs_dir += "-passage"
+    else:
+        raise ValueError("passage_prefix should be one of", passage_prefixes_e5)
+
+
+working_dir_embs_path = Path("/".join([args.working_dir, embs_dir, args.name, model_name_for_embs_dir]))
+working_dir_embs_path.mkdir(parents=True, exist_ok=True)
+
+print("output embs path:", working_dir_embs_path)
 
 MODEL = SentenceTransformer(emb_config.model_name)
 MODEL.max_seq_length = emb_config.max_seq_length
 
 
-def to_embs(texts: list[str], group_size=1024) -> Generator[np.ndarray, None, None]:
+def to_embs(texts: list[str], group_size=1024, model=MODEL) -> Generator[np.ndarray, None, None]:
     group = []
     for text in texts:
         group.append(text)
         if len(group) == group_size:
-            embeddings = MODEL.encode(
+            embeddings = model.encode(
                 group,
                 normalize_embeddings=True,
                 show_progress_bar=False,
@@ -140,25 +150,10 @@ def to_embs(texts: list[str], group_size=1024) -> Generator[np.ndarray, None, No
             yield embeddings  # type: ignore
             group = []
     if len(group) > 0:
-        embeddings = MODEL.encode(
+        embeddings = model.encode(
             group, normalize_embeddings=True, show_progress_bar=False
         )
         yield embeddings  # type: ignore
-
-
-def _to_data_text(
-    data, prefix=emb_config.passage_prefix, max_len=int(emb_config.max_seq_length * 1.5)
-):
-    return (prefix + data["title"] + "\n" + data["text"])[0:max_len]
-
-
-def _to_chunk_text(
-    data, prefix=emb_config.passage_prefix, max_len=int(emb_config.max_seq_length * 1.5)
-):
-    return (prefix + data["title"] + "\n" + data["overlap_text"] + data["text"])[
-        :max_len
-    ]
-
 
 def ds_to_embs(
     ds,
@@ -192,21 +187,20 @@ elif torch.backends.mps.is_available(): # type: ignore
 else:
     print("!! Warning: use cpu")
 
-ds = load_dataset(args.target)["train"]  # type: ignore
-to_text = _to_data_text if args.target == "data" else _to_chunk_text
+ds = target_ds # type: ignore
 
 if args.debug:
     print("debug mode")
     ds = ds.select(range(19998))  # type: ignore
     print("small dataset len:", len(ds))
-    group_size = 10000
+    group_size = 10_000
 else:
-    print("dataset len:", len(ds))
+    print("dataset len:", len(ds)) # type: ignore
     group_size = 100_000
 
-for embs, idx, pbar in ds_to_embs(ds, to_text, group_size=group_size):
+for embs, idx, pbar in ds_to_embs(ds, data_to_passage, group_size=group_size):
     filename = f"{idx}.npz"
-    filepath = output_embs_path / filename
+    filepath = working_dir_embs_path / filename
     pbar.desc = f"saving...: {str(filepath)}"
     np.savez_compressed(filepath, embs=embs.astype(np.float16))
     pbar.desc = ""
