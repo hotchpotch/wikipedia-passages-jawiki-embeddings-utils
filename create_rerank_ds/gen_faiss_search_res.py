61e096f2e7372019339b7e2ab8645a02b92b8c54
Yuichi Tateno <hotchpotch@gmail.com>
2024-02-20 19:54:51 +0900

    update

--- create_rerank_ds/gen_faiss_search_res.py
+++ create_rerank_ds/gen_faiss_search_res.py
@@ -1,5 +1,6 @@
 """
-faissで検索した結果をまず作成する
+1. faissで検索した結果をまず作成する
+
 """
 
 from __future__ import annotations
@@ -31,12 +32,6 @@ RERANK_DS_WORK_DIR = PROJECT_ROOT / "workdir/rerank_ws"
 RERANK_DS_WORK_DIR.mkdir(parents=True, exist_ok=True)
 
 
-JAQKET_V1_DEV_URLS = [
-    "https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_01/dev1_questions.json",
-    "https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_01/dev2_questions.json",
-]
-
-
 WIKIPEDIA_JA_DS = "singletongue/wikipedia-utils"
 WIKIPEDIA_JS_DS_NAME = "passages-c400-jawiki-20230403"
 WIKIPEDIA_JA_EMB_DS = "hotchpotch/wikipedia-passages-jawiki-embeddings"
@@ -47,7 +42,7 @@ EMB_MODEL_PQ = {
     "intfloat/multilingual-e5-large": 256,
     "cl-nagoya/sup-simcse-ja-base": 192,
     "pkshatech/GLuCoSE-base-ja": 192,
-    "text-embedding-3-small-dim512": 128,
+    # "text-embedding-3-small-dim512": 128,
 }
 
 EMB_MODEL_NAMES = list(EMB_MODEL_PQ.keys())
@@ -163,90 +158,13 @@ def faiss_search_by_embs(faiss_index, embs, top_k=5):
     return D, I, search_sec
 
 
-# jaqket v1
-@dataclass
-class JaqketQuestionV1:
-    qid: str
-    question: str
-    answer_entity: str
-    label: int
-    answer_candidates: list[str]
-    original_question: str
-
-
-def load_jaqket_v1(urls):
-    res: list[JaqketQuestionV1] = []
-    for url in urls:
-        with urllib.request.urlopen(url) as f:
-            # f は 1行ごとに処理
-            data = [json.loads(line.decode("utf-8")) for line in f]
-        for d in data:
-            # label position
-            d["label"] = d["answer_candidates"].index(d["answer_entity"])
-            # if -1
-            if d["label"] == -1:
-                raise ValueError(
-                    f"answer_entity not found in answer_candidates: {d['answer_entity']}, {d['answer_candidates']}"
-                )
-            res.append(
-                JaqketQuestionV1(
-                    qid=d["qid"],
-                    question=d["question"],
-                    answer_entity=d["answer_entity"],
-                    label=d["label"],
-                    answer_candidates=d["answer_candidates"],
-                    original_question=d["original_question"],
-                )
-            )
-    return res
-
-
-def find_label_by_indexes(idxs, jaqket: JaqketQuestionV1, wiki_ds) -> int:
-    INT_MAX = 2**31 - 1
-    answer_candidates = jaqket.answer_candidates
-    candidate_found_indexes = []
-    titles = []
-    texts = []
-    for idx in idxs[0:top_k]:
-        data = wiki_ds[idx]
-        title = data["title"]
-        text = data["text"]
-        titles.append(title)
-        texts.append(text)
-    target_text = " ".join(titles + texts)
-    for candidate in answer_candidates:
-        pos = target_text.find(candidate)
-        if pos == -1:
-            candidate_found_indexes.append(INT_MAX)
-        else:
-            candidate_found_indexes.append(pos)
-    min_index = min(candidate_found_indexes)
-    if min_index == INT_MAX:
-        return -1
-    else:
-        return candidate_found_indexes.index(min_index)
-
-
-def predict_by_indexes(indexes, jaqket_ds, wiki_ds):
-    pred_labels = []
-    total = len(indexes)
-    for idxs, jaqket in tqdm(zip(indexes, jaqket_ds), total=total):
-        # tolist がある場合は実行する
-        if hasattr(idxs, "tolist"):
-            idxs = idxs.tolist()
-        pred_label = find_label_by_indexes(idxs, jaqket, wiki_ds)
-        pred_labels.append(pred_label)
-    return pred_labels
-
-
 print("load wikija datasets")
 ds = get_wikija_ds()
 
-jacket_v1 = load_jaqket_v1(JAQKET_V1_DEV_URLS)
+jacket_ds = load_dataset("hotchpotch/jaqket_cc")
 
 if parsed_args.debug:
     print("RUN: debug mode")
-    jacket_v1 = jacket_v1[:100]
 
 if parsed_args.emb_model_name:
     target_emb_models = [parsed_args.emb_model_name]
@@ -255,13 +173,7 @@ else:
 
 use_gpu = parsed_args.use_gpu
 
-# rerankingは e5 でのみ実行
-if parsed_args.reranking:
-    print("reranking is e5 only")
-    target_emb_models = [m for m in target_emb_models if "e5" in m]
-    print("reranking target_emb_models: ", target_emb_models)
-
-results = []
+result_targets = {}
 
 for emb_model_name in target_emb_models:
     if "-e5-" in emb_model_name:
@@ -289,26 +201,34 @@ for emb_model_name in target_emb_models:
     index_name = f"{index_emb_model_name}/index_IVF2048_PQ{emb_model_pq}.faiss"
     faiss_index = get_faiss_index(index_name, use_gpu=use_gpu)
 
-    jaqket = jacket_v1
-    question_embs, gen_embs_sec = texts_to_embs(
-        model_to_embs_fn,
-        texts=[q.question for q in jaqket],
-        prefix=search_text_prefix,
-    )
-    gen_embs_sec = round(gen_embs_sec, 2)
-    print("question_embs.shape: ", question_embs.shape)  # type: ignore
-    print("gen embs sec: ", gen_embs_sec)
-    scores, indexes, search_sec = faiss_search_by_embs(
-        faiss_index, question_embs, top_k=SEARCH_TOP_K
-    )
-    # 一旦、検索結果を保存する
-    results.append(
-        {
-            "name": name,
-            "indexes": indexes,
-            "scores": scores,
-        }
-    )
+    for jacket_target in list(jacket_ds.keys()):  # type: ignore
+        jaqket = jacket_ds[jacket_target]  # type: ignore
+        if parsed_args.debug:
+            jaqket = jaqket.select(range(30))  # top-30
+        question_embs, gen_embs_sec = texts_to_embs(
+            model_to_embs_fn,
+            texts=jaqket["question"],
+            prefix=search_text_prefix,
+        )
+        gen_embs_sec = round(gen_embs_sec, 2)
+        print("question_embs.shape: ", question_embs.shape)  # type: ignore
+        print("gen embs sec: ", gen_embs_sec)
+        scores, indexes, search_sec = faiss_search_by_embs(
+            faiss_index, question_embs, top_k=SEARCH_TOP_K
+        )
+        # 一旦、検索結果を保存する
+        if jacket_target not in result_targets:
+            result_targets[jacket_target] = []
+        result_targets[jacket_target].append(
+            {
+                "name": name,
+                "qids": jaqket["qid"],
+                "indexes": indexes,
+                "scores": scores,
+                "questions": jaqket["question"],
+                "answers": jaqket["answers"],
+            }
+        )
     del faiss_index
     torch.cuda.empty_cache()
     del model_to_embs_fn
@@ -323,23 +243,4 @@ else:
     filename = f"search_results.pkl.gz"
 
 with open(RERANK_DS_WORK_DIR / filename, "wb") as f:
-    pickle.dump(results, f)
-
-# # 最後に results を df にして、table で全て表示
-# pd.set_option("display.max_rows", None)
-# df = pd.DataFrame(results)
-# print(df)
-
-# # csv で保存
-# if parsed_args.debug:
-#     filename = f"eval_jaqket_{jacket_target}_debug"
-# else:
-#     filename = f"eval_jaqket_{jacket_target}"
-
-# if parsed_args.cross_encoder_reranking:
-#     filename_ce_name = parsed_args.cross_encoder_reranking.split("/")[-1]
-#     filename += "_ce_" + filename_ce_name
-
-# filename = filename + ".csv"
-# print("save csv: ", filename)
-# df.to_csv(filename, index=False)
+    pickle.dump(result_targets, f)
